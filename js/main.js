728674b6fe5bf32bcc90b2fa0c2c31882d494794
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-21 15:56:49 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js');
 }
 
+  
 document.querySelector('#show').addEventListener('click', () => {
   const iconUrl = document.querySelector('select').selectedOptions[0].value;
   let imgElement = document.createElement('img');

f453d171c5a909ee0578712c563bff46ea15f8ba
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-22 15:59:11 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 function regSW() {
  'use strict';
-  
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
-  }
+ 
+ if (!('serviceWorker' in navigator)) {
+  console.log('Service Worker not supported');
+  return;
+}
+navigator.serviceWorker.register('/service-worker.js')
+.then(function(registration) {
+  console.log('SW registered! Scope is:', registration.scope);
+}); // .catch a registration error
+ 
 }

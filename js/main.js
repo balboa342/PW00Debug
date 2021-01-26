46fe37831acfd93204f11f6dc1499cd5d978b3f5
balboa342 <42502796+balboa342@users.noreply.github.com>
2021-01-26 16:42:23 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,3 @@ function regSW(){
   }
 }
 
-/*
-document.querySelector('#show').addEventListener('click', () => {
-  const iconUrl = document.querySelector('select').selectedOptions[0].value;
-  let imgElement = document.createElement('img');
-  imgElement.src = iconUrl;
-  document.querySelector('#container').appendChild(imgElement);
-});
-}
-
-*/
